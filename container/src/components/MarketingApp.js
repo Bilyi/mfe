5c80ba70070ccc1435e325d4740bd1e987dccc04
sbilyi <Sbil141288z>
2020-12-10 11:23:50 +0200

    initial commit

---
+++ container/src/components/MarketingApp.js
@@ -0,0 +1,16 @@
+import React, { useEffect, useRef } from 'react';
+import { mount } from 'marketing/Marketing';
+
+const MarketingApp = () => {
+    const ref = useRef(null);
+
+    useEffect(() => {
+        mount(ref.current);
+    });
+
+    return (
+        <div ref={ref} />
+    )
+};
+
+export default MarketingApp;
