d8dc025d2522be788fde61e31a684bf7ce4b8dc3
sbilyi <Sbil141288z>
2020-12-12 00:22:08 +0200

    test

--- container/src/components/MarketingApp.js
+++ container/src/components/MarketingApp.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useRef } from 'react';
 import { mount } from 'marketing/MarketingApp';
+import React, { useEffect, useRef } from 'react';
 
 const MarketingApp = () => {
     const ref = useRef(null);
